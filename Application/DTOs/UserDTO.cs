f25f99fdf89cea05f4970f54a9258704279d92b0
BriceNogue <bricenogue@gmail.com>
2025-04-14 15:50:01 +0200

    UserDTO

--- Application/DTOs/UserDTO.cs
+++ Application/DTOs/UserDTO.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs
 {
-    public class UserDTO
+    public class UserDTO : _BaseDTO
     {
+        public string Firstname { get; set; } = string.Empty;
+        public string Lastname { get; set; } = string.Empty;
+        public string Phone_number { get; set; } = string.Empty;
+        public string Email_address { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Role { get; set; }
     }
 }
